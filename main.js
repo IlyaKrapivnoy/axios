41232fc40dbebcb9d8876ee98c6ea5d09fd81853
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-06 20:27:15 +0200

    error handling

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// AXIOS GLOBALS
+axios.defaults.headers.common['X-Auth-Token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
 // GET REQUEST
 function getTodos() {
     axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
@@ -96,7 +99,27 @@ function getTodos() {
   
   // ERROR HANDLING
   function errorHandling() {
-    console.log('Error Handling');
+    axios
+      .get('https://jsonplaceholder.typicode.com/todoss', {
+      })
+      .then(res => showOutput(res))
+      .catch(err => {
+        if (err.response) {
+          // Server responded with a status other than 200 range
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+  
+          if (err.response.status === 404) {
+            alert('Error: Page Not Found');
+          }
+        } else if (err.request) {
+          // Request was made but no response
+          console.error(err.request);
+        } else {
+          console.error(err.message);
+        }
+      });
   }
   
   // CANCEL TOKEN
